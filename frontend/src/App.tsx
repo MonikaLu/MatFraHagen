000e9d5a74e7da7048f00b8c8c964e6ff850a13f
aashiltf <aas-ten@hotmail.com>
2024-08-27 14:29:53 +0200

    Add tailwind config and UI structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold underline">Mat Fra Hagen!!</h1>
+      <div className="flex">
+        <h2 className="p-4 text-center">LOGO</h2> 
+        <h1 className="p-4 text-center">Mat Fra Hagen</h1>
+        <button className="bg-button text-buttonText p-4 text-center ml-auto">Logg inn</button>
+      </div>
+      <div className="">Søk etter område</div>
+      <div>Her kommer view av annonser</div>
     </>
   );
 }
