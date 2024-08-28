fbc5c877eb6eee4de5a4efa38530c15261d69f3d
aashiltf <aas-ten@hotmail.com>
2024-08-28 12:21:12 +0200

    Add UI for homepage with counter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [count, setCount] = useState<number>(0);
+  const increment = () => {
+    setCount(count + 1);
+  }
+
   return (
     <>
       <div className="flex">
         <h2 className="p-4 text-center">LOGO</h2> 
         <h1 className="p-4 text-center">Mat Fra Hagen</h1>
-        <button className="bg-button text-buttonText p-4 text-center ml-auto">Logg inn</button>
+        <button onClick={increment} className="bg-button text-buttonText p-4 text-center ml-auto">Logg inn ({count})</button>
       </div>
       <div className="">Søk etter område</div>
       <div>Her kommer view av annonser</div>
