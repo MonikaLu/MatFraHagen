ec711500ee772818307199b582f3a3fa7ec55d22
aashiltf <aas-ten@hotmail.com>
2024-08-29 13:02:15 +0200

    #9 Add AdGrid component and mock data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import AdGrid from "./components/AdGrid";
 
 function App() {
   return (
     <>
       <div className="flex">
-        <h2 className="p-4 text-center">LOGO</h2> 
+        <h2 className="p-4 text-center">LOGO</h2>
         <h1 className="p-4 text-center">Mat Fra Hagen</h1>
-        <button className="bg-button text-buttonText p-4 text-center ml-auto">Logg inn</button>
+        <button className="bg-button text-buttonText p-3 text-center ml-auto text-paragraph1 rounded-md">
+          Logg inn
+        </button>
       </div>
       <div className="">Søk etter område</div>
-      <div>Her kommer view av annonser</div>
+      <AdGrid />
     </>
   );
 }
